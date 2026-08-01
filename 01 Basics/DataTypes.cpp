f24a1f7f91762417099f16c184837c8efbf58b5a
Tapash Bharali <137592576+Tapash-Bharali@users.noreply.github.com>
2026-08-01 10:15:59 +0530

    Add files via upload

---
+++ 01 Basics/DataTypes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  // int x = 10;
+  // long b = 15;
+  // cin >> x;
+
+  // long long c = 1500000000000;
+
+  float y = 5.6;
+  float z = 5;
+  cout<<"Value of y:" << y;
+
+  // string, getline 
+
+  string s1,s2;
+  cin>>s1>>s2;
+  cout<<s1 << " " <<s2;
+  return 0;
+}
